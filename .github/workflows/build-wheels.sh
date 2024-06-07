3c8e3e8e76aa05e1f78ab533f2cbf70449e6e636
tujitu-umn <141323926+tujitu-umn@users.noreply.github.com>
2024-06-07 17:46:48 +0900

    workflow

---
+++ .github/workflows/build-wheels.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e -x
+
+for PYBIN in /opt/python/cp3[89]*/bin /opt/python/cp310*/bin; do
+    "${PYBIN}/pip" install maturin
+    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
+done
+
+mkdir -p wheelhouse
+for wheel in target/wheels/*.whl; do
+    auditwheel repair "${wheel}" -w wheelhouse/
+done
+
+# Move repaired wheels back to target/wheels
+mv wheelhouse/*.whl target/wheels/
+
+# Publish the wheels to PyPI
+maturin publish --username __token__ --password "${PYPI_API_TOKEN}"
